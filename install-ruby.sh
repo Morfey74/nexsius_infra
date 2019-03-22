55da799576b26342a732a78968abcba3fd6ff36b
Sergey Nex <nexsius@gmail.com>
2019-03-22 23:17:56 +0300

    edit startup.sh

--- install-ruby.sh
+++ install-ruby.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 echo "Installing ruby..." > log
 sudo apt update && sudo apt install -y ruby-full ruby-bundler build-essential
-if [ $? -eq 0 ]; then
-   echo "ruby successfuly installed" >> log
-fi
