0483442bbef1a1cf93f92a9a26be8299dc569a2c
Sergey Nex <nexsius@gmail.com>
2019-04-03 12:52:38 +0300

    add variables for zone and key

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,55 +1,60 @@
 terraform {
-	required_version = "0.11.13"
+  required_version = "0.11.13"
 }
 
 provider "google" {
-	version = "2.0.0"
-	project = "${var.project}"
-	region = "${var.region}"
+  version = "2.0.0"
+  project = "${var.project}"
+  region  = "${var.region}"
 }
 
 resource "google_compute_instance" "app" {
-	name = "reddit-app"
-	machine_type = "g1-small"
-	zone = "europe-west1-b"
-	tags = ["reddit-app"]
-	
-	boot_disk {
-		initialize_params {
-			image = "${var.disk_image}"
-		}
-	}
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
 
-        metadata {
-        	ssh-keys = "appuser:${file(var.public_key_path)}"
-	}
+  boot_disk {
+    initialize_params {
+      image = "${var.disk_image}"
+    }
+  }
 
-	network_interface {
-		network = "default"
-		access_config {}		
-	}		
-	connection {
-		type = "ssh"
-		user = "appuser"
-		agent = "false"
-		private_key = "${file("~/.ssh/appuser")}"
-	}
-	provisioner "file" {
-		source = "files/puma-server.service"
-		destination = "/tmp/puma-server.service"
-	}
-	provisioner "remote-exec" {
-		script="files/deploy.sh"
-	}
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = "false"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "file" {
+    source      = "files/puma-server.service"
+    destination = "/tmp/puma-server.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-	name = "allow-puma-default"
-	network = "default"
-	allow {
-		protocol = "tcp"
-		ports = ["9292"]
-	}
-	source_ranges = ["0.0.0.0/0"]
-	target_tags = ["reddit-app"]
+  name    = "allow-puma-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,25 @@
 variable project {
-	description = "Project ID"
+  description = "Project ID"
+}
+
+variable zone {
+  description = "GCP zone"
+  default     = "europe-west1-b"
 }
 
 variable region {
-	description = "Region"
-	default = "europe-west1"
+  description = "Region"
+  default     = "europe-west1"
 }
 
 variable public_key_path {
-	description = "Path to the public key used for ssh access"
+  description = "Path to the public key used for ssh access"
 }
 
 variable disk_image {
-	description = "Disk image"
+  description = "Disk image"
 }
 
+variable private_key_path {
+  description = "Path to private key"
+}
