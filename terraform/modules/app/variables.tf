78d4417adaa7518b0abae618a7e81d79413e821a
Sergey Nex <nexsius@gmail.com>
2019-04-15 15:47:28 +0300

    minor fix

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -7,6 +7,8 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
+variable private_key_path {}
+
 variable app_disk_image {
   description = "Disk image reddit app"
   default = "reddit-app-base"
